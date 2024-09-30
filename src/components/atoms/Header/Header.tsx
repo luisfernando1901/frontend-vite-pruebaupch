c09c877261378a4a69d8ea15bb2c283a7e7072b3
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2024-09-30 17:59:20 -0500

    Reagrupar componentes y su respectiva logica.

---
+++ src/components/atoms/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="d-flex flex-row justify-content-center header py-3">
+      <img
+        src="https://logo.clearbit.com/clearbit.com"
+        alt="Logo de la empresa"
+        className="d-inline-block align-text-top"
+        height="30"
+      />
+    </div>
+  );
+};
+
+export default Header;
